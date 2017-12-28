adeb6c8946ad02822349cc2f2d4f5b0085fc6913
xpleaf <xpleaf@163.com>
2017-12-29 00:57:25 +0800

    web31 shop04

---
+++ WEB31_shop04/HeimaShop/src/com/itheima/service/UserService.java
@@ -0,0 +1,51 @@
+package com.itheima.service;
+
+import java.sql.SQLException;
+
+import com.itheima.dao.UserDao;
+import com.itheima.domain.User;
+
+public class UserService {
+
+	public boolean regist(User user) {
+
+		UserDao dao = new UserDao();
+		int row = 0;
+		try {
+			row = dao.regist(user);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return row>0?true:false;
+	}
+
+	//激活
+	public void active(String activeCode) {
+		UserDao dao = new UserDao();
+		try {
+			dao.active(activeCode);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	//校验用户名是否存在
+	public boolean checkUsername(String username) {
+		UserDao dao = new UserDao();
+		Long isExist = 0L;
+		try {
+			isExist = dao.checkUsername(username);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return isExist>0?true:false;
+	}
+
+	//用户登录的方法
+	public User login(String username, String password) throws SQLException {
+		UserDao dao = new UserDao();
+		return dao.login(username,password);
+	}
+
+}
---
+++ WEB31_shop04/HeimaShop/src/com/itheima/utils/CommonsUtils.java
@@ -0,0 +1,12 @@
+package com.itheima.utils;
+
+import java.util.UUID;
+
+public class CommonsUtils {
+
+	//生成uuid方法
+	public static String getUUID(){
+		return UUID.randomUUID().toString();
+	}
+	
+}
